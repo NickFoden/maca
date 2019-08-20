d6724db47f6a79800253323bfc681c1d9f9d5031
Nick Foden <nickfoden@gmail.com>
2019-08-19 23:38:03 -0400

    Add footer accordion

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { Fragment, useState} from "react";
 import { Link } from "react-router-dom";
 import Social from "../components/Social";
+import chevron from "../assets/arrow.svg"
 import "./styles.scss";
 
 const Footer = props => {
+
+  const [orderVisible, toggleOrderVisible] = useState(false);
+  const [companyVisible, toggleCompanyVisible] = useState(false);
+
+  const toggleOrderAccordion = () => {
+    toggleOrderVisible(!orderVisible,);
+  };
+  const toggleCompanyAccordion = () => {
+    toggleCompanyVisible(!companyVisible);
+  };
   return (
     <footer className="footer_container">
       <div className="footer_left_container">
@@ -23,6 +34,24 @@ const Footer = props => {
         <h3>Order</h3>
         <Link to="/tracker">Order Tracker</Link>
         <Link to="/delivery">Delivery FAQs</Link>
+      </nav>
+
+      <nav className="footer_nav_child_div_mobile">
+        <button onClick={toggleCompanyAccordion}><h3>Company</h3> {companyVisible ? <img className="chevron_arrow" src={chevron} alt="arrow up" /> : <img className="chevron_arrow"  src={chevron} alt="arrow down" style={{transform:'rotate(180deg)'}}/>}</button>
+        {companyVisible && 
+        <Fragment>
+        <Link to="/about">About</Link>
+        <Link to="/locations">Locations</Link>
+        <Link to="/contact">Contact Us</Link>
+        </Fragment>}
+      </nav>
+      <nav className="footer_nav_child_div_mobile">
+        <button onClick={toggleOrderAccordion}><h3>Order</h3> {orderVisible ? <img className="chevron_arrow" src={chevron} alt="arrow up"/> : <img className="chevron_arrow" src={chevron} alt="arrow down" style={{transform:'rotate(180deg)'}}/>}</button>
+        {orderVisible && 
+        <Fragment>
+        <Link to="/tracker">Order Tracker</Link>
+        <Link to="/delivery">Delivery FAQs</Link>
+        </Fragment>}
       </nav>
     </footer>
   );
