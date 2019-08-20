6e09001907675dddce15a83abb7cc1064f6eecd7
Nick Foden <nickfoden@gmail.com>
2019-08-19 23:41:07 -0400

    Update readme add some opacity to chevrons

--- src/utils/currencyConvert.js
+++ src/utils/currencyConvert.js
@@ -1,7 +1,7 @@
 export const formatCurrency = (amount, cart) => {
   const result = (amount / 100).toFixed(2);
-
   //If no cents just return the dollars
+  //But display with cents if in cart
   if (!cart) {
     if (result.toString().endsWith("00")) {
       return (amount / 100).toFixed(0);
